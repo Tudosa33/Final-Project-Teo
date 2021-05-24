f44bda901616f53dc0574fcd193f010418133c7b
Your Name <you@example.com>
2021-05-24 16:03:44 +0300

    modifica js-ul

--- Pagina-3/js/script.js
+++ Pagina-3/js/script.js
@@ -5,58 +5,6 @@ const prevButton = document.querySelector(".carosel_button--left");
 const dotsNav = document.querySelector(".carosel-nav");
 const dots = Array.from(dotsNav.children);
 
-const slideWidth = slides[0].getBoundingClientRect().width;//getBoundingClientRec ia dimensiunea elemetului
-
-const setSlidePosition = (slide, index) => {
-    slide.style.left = slideWidth * index + "px"
-}
-slides.forEach(setSlidePosition);
-
-
-function moveToSlide(track, currentSlide, targetSlide) {
-    track.style.transform = 'translateX(' + targetSlide.style.left + ')';
-    currentSlide.classList.remove("current-slide")
-    targetSlide.classList.add("current-slide")
-}
-
-function updateDots(currentDot, targetDot) {
-    currentDot.classList.remove("current-slide");
-    targetDot.classList.add('current-slide');
-}
-
-prevButton.addEventListener("click", function(e) {
-    const currentSlide = track.querySelector(".current-slide");
-    const prevSlide = currentSlide.previousElementSibling;// merge la elementul de dinainte
-    const currentDot = dotsNav.querySelector(".current-slide");
-    const prevDot = currentDot.previousElementSibling;
-
-    moveToSlide(track, currentSlide, prevSlide);
-    updateDots(currentDot, prevDot)
-
-})
-
-nextButton.addEventListener("click", function(e) {
-    const currentSlide = track.querySelector(".current-slide");
-    const nextSlide = currentSlide.nextElementSibling; // merge la urmatorul slide
-    const currentDot = dotsNav.querySelector(".current-slide");
-    const nextDot = currentDot.nextElementSibling;
-
-    moveToSlide(track, currentSlide, nextSlide);
-    updateDots(currentDot, nextDot)
-})
-
-dotsNav.addEventListener('click', function(e) {
-    const targetDot = e.target.closest('button');
-
-    const currentSlide = track.querySelector(".current-slide");
-    const currentDot = dotsNav.querySelector(".current-slide");
-    const targetIndex = dots.findIndex(dot => dot === targetDot);
-    const targetSlide = slides[targetIndex]
-
-    moveToSlide(track, currentSlide, targetSlide);
-
-    updateDots(currentDot, targetDot)
-});
 
 
 
